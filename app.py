4018bc81460318acc74f4d1a3c7a47ae9644b2d7
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-12-15 15:51:05 +0100

    Added scrapping for company names

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask
 from flask import request
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 app = Flask(__name__)
+
+def scrape_for(query):
+    url = "https://panoramafirm.pl/szukaj?k={}".format(query)
+    company_selector = "#company-list li.company-item"
+    company_name_selector = "a.company-name"
+    
+    response = urlopen(url)
+    html = BeautifulSoup(response.read())
+    selected_elements = html.select(company_selector)
+    
+    out_str = ""
+    
+    for e in selected_elements:
+        out_str += e.select(company_name_selector)[0].getText() + "<br />"
+        
+    return out_str
 
 def page_with(content):
     return """
@@ -28,7 +46,8 @@ def index():
  
 @app.route("/search")
 def search():
-    return page_with(request.args.get("query"))
+    query = request.args.get("query")
+    return page_with("<code>" + scrape_for(query) + "</code>")
 
 @app.route("/vcard/<url>")
 def vcard(url):
