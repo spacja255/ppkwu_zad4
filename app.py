b3dbb8187f7a01d5196e5300a802c13c3c0ac420
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-12-15 15:34:23 +0100

    Added index page with search form

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
+def page_with(content):
+    return """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Mobilny vCard</title>
+        </head>
+        <body>
+            """ + content + """
+        </body>
+        </html>
+    """
+
 @app.route("/")
 def index():
-    return ""
+    return page_with("""
+        <form method="get" action="/search">
+            <input type="text" name="query" placeholder="Enter query..." />
+            <input type="submit" value="Search" />
+        </form>
+    """)
  
-@app.route("/search/<query>")
-def search(query):
-    return ""
+@app.route("/search")
+def search():
+    return page_with(request.args.get("query"))
 
 @app.route("/vcard/<url>")
 def vcard(url):
